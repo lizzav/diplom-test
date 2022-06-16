907ec2a262ba9f0e7b3d3092fb2d9e331d7eecf7
lizazav <liziktele2@gmail.com>
2022-06-16 16:56:40 +0700

    initial commit

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -7,6 +7,8 @@ class BrandController {
     return res.json(brand);
   }
   async getAll(req, res) {
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token,req.user)
     const brand = await Brand.findAll();
     return res.json(brand);
   }
--- server/models/models.js
+++ server/models/models.js
@@ -5,7 +5,8 @@ const User = sequelize.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
-  role: { type: DataTypes.STRING, defaultValue: "USER" }
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+  name: { type: DataTypes.STRING }
 });
 const Basket = sequelize.define("basket", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
@@ -31,11 +32,6 @@ const Brand = sequelize.define("brand", {
   name: { type: DataTypes.STRING, unique: true, allowNull: false }
 });
 
-const Rating = sequelize.define("rating", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  rate: { type: DataTypes.INTEGER, allowNull: false }
-});
-
 const DeviceInfo = sequelize.define("device_info", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   title: { type: DataTypes.STRING, allowNull: false },
@@ -45,6 +41,62 @@ const DeviceInfo = sequelize.define("device_info", {
 const TypeBrand = sequelize.define("type_brand", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
 });
+
+const Travel = sequelize.define("travel", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false }
+});
+
+const TravelSight = sequelize.define("travel-sight", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  visited:{type:DataTypes.BOOLEAN, defaultValue:false},
+});
+
+const Sight = sequelize.define("sight", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
+  lat: { type: DataTypes.INTEGER },
+  lng: { type: DataTypes.INTEGER },
+  address: { type: DataTypes.STRING, allowNull: false },
+});
+const City = sequelize.define("city", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
+  lat: { type: DataTypes.INTEGER },
+  lng: { type: DataTypes.INTEGER }
+});
+const Country = sequelize.define("country", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
+  lat: { type: DataTypes.INTEGER },
+  lng: { type: DataTypes.INTEGER }
+});
+
+const Photo = sequelize.define("photo", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false }
+});
+
+const Rating = sequelize.define("rating", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  review: { type: DataTypes.STRING },
+  grade: { type: DataTypes.INTEGER, allowNull: false  }
+});
+const Error = sequelize.define("error", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
+  verified:{type:DataTypes.BOOLEAN, defaultValue:false},
+  email: { type: DataTypes.STRING},
+});
+const Like = sequelize.define("like", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  like:{type:DataTypes.BOOLEAN},
+});
+
 //описываем связи
 User.hasOne(Basket);
 Basket.belongsTo(User);
@@ -67,20 +119,66 @@ Rating.belongsTo(Device);
 Device.hasMany(BasketDevice);
 BasketDevice.belongsTo(Device);
 
-Device.hasMany(DeviceInfo, {as:'info'});
+Device.hasMany(DeviceInfo, { as: "info" });
 DeviceInfo.belongsTo(Device);
 
 Type.belongsToMany(Brand, { through: TypeBrand });
 Brand.belongsToMany(Type, { through: TypeBrand });
 
+
+//new
+User.hasMany(Travel);
+Travel.belongsTo(User);
+
+User.hasMany(Like);
+Like.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Rating.hasMany(Like);
+Like.belongsTo(Rating);
+
+Sight.hasMany(Rating);
+Rating.belongsTo(Sight);
+
+
+Sight.hasMany(Photo);
+Photo.belongsTo(Sight);
+
+
+City.hasMany(Sight);
+Sight.belongsTo(City);
+
+
+Country.hasMany(City);
+City.belongsTo(Country);
+
+Travel.belongsToMany(Sight, { through: TravelSight });
+Sight.belongsToMany(Travel, { through: TravelSight });
+
+
+
 module.exports = {
   Basket,
-  User,
+  // User,
   BasketDevice,
   Device,
   Type,
   Brand,
-  Rating,
+  // Rating,
   DeviceInfo,
-  TypeBrand
+  TypeBrand,
+  //new
+  User,
+  Travel,
+  TravelSight,
+  Sight,
+  Country,
+  City,
+  Photo,
+  Rating,
+  Like,
+  Error
+
 };
